983f72c33f678ef9c4741f2d3d2ffd8291312466
Nifemi <nifemiadeoluwa2006@gmail.com>
2022-09-29 15:24:16 +0100

    done

---
+++ index.js
@@ -0,0 +1,71 @@
+        //PART B//
+
+//1
+
+var students =[['David', 50], ['Emma', 60], ['Thomas', 80], ['king', 95],]
+
+var Avgscore = 45;
+
+for (var i=45; i < students.length; i++) {
+     Avgscore += students[i]
+     [50];
+     var avg =( Avgscore/students.length)
+}
+console.log("Average garde:" + (Avgscore)/students.length);
+
+if(avg < 50){
+     console.log("Grade : F ");
+}else if (avg < 60) {
+     console.log("Grade : D");
+}else if (avg < 70) {
+     console.log("Grade : C");
+}else if(avg < 80) {
+     console.log("Grade : B");
+}else if(avg < 100) {
+     console.log("Grade : A");
+}
+
+
+          //PART C
+
+//1
+
+const land = [
+     'FinLand',
+     'Ireland',
+]
+
+//2
+
+const countries = [
+     'Japan'
+]
+
+//7
+
+const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey'];
+
+shoppingCart.unshift("Meat")
+
+//7 b
+
+shoppingCart.push("Sugar")
+
+//7c
+
+shoppingCart.pop();
+
+
+//9
+
+const fullstackDeveloper = frontend.concat(backend);
+
+
+
+// PART C
+
+//1
+
+//The difference between the callback functions is foreach loop over the array and exectues the callback and check its return value.
+
+
